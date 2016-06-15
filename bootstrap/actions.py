d97c492b261a154e1aae757d5d518294e3f71fd9
Dylan Vuz <vuzdylan@gmail.com>
2016-06-14 22:00:31 -0700

    update actions gen

--- bootstrap/actions.py
+++ bootstrap/actions.py
@@ -5,14 +5,18 @@ args = sys.argv
 
 if len(args) == 2:
     with open(os.path.abspath('.') + "/app/js/actions/" + args[1] + ".js", "w+") as file:
-        n = raw_input('Enter action name or q to exit > ')
+        print("Enter actions as 'action name - data attr'")
+        print("This will create actionName(data, attr)")
+        print("q or Q to create file")
+        n = raw_input('> ')
         actions = {}
         while True:
             if n in ['q', 'Q']:
                 break
-            m = raw_input('Enter data types or leave blank > ')
-            actions[n] = m.split(' ')
-            n = raw_input('Enter action name or q to exit > ')
+            action = n.split('-')[0]
+            data = n.split('-')[1].lstrip().split(' ')
+            actions[action] = data
+            n = raw_input('> ')
 
         file.write("'use strict';\n\n")
         for key, val in actions.iteritems():
@@ -31,4 +35,5 @@ if len(args) == 2:
             file.write(data)
             file.write("  };\n")
             file.write("}\n\n")
-
+else:
+    print("arg - action file name")
