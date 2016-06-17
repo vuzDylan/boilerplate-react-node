5cf80cbf25a2460cd007a58b841bff1d08488dc0
Dylan Vuz <vuzdylan@gmail.com>
2016-06-16 21:12:31 -0700

    gulp watch is way better than just watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,10 +2,19 @@
 
 import gulp from 'gulp';
 import paths from  '../config';
+import watch from 'gulp-watch';
 
 gulp.task('watch', ['build'], () => {
-  gulp.watch(paths.source.scripts, ['build:scripts']);
-  gulp.watch([paths.source.css], ['build:css']);
-  gulp.watch([paths.source.html], ['build:html']);
-  gulp.watch([paths.source.img], ['build:img']);
+  watch(paths.source.scripts, () => {
+    gulp.start('build:scripts');
+  });
+  watch(paths.source.css, () => {
+    gulp.start('build:css');
+  });
+  watch(paths.source.html, () => {
+    gulp.start('build:html');
+  });
+  watch(paths.source.img, () => {
+    gulp.start('build:img');
+  });
 });
