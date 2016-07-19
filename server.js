aea7ef822e4c2a0da8b5d838c645b0e23b03baa9
Dylan Vuz <vuzdylan@gmail.com>
2016-07-18 20:25:37 -0700

    read server from gulp config

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 var path = require('path');
 var express = require('express');
+var config = require('./gulp/config').default;
 
 const app = express();
 
-app.use('/', express.static('dist'));
+app.use('/', express.static(config.build.dir));
 
 app.use('/*', function(req, res){
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, (config.build.dir), 'index.html'))
 });
 
 module.exports =  app;
